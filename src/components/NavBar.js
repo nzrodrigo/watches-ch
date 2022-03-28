8be75f9b7a1c17bd69e543629b3ec2fd98028dcc
Rodrigo <rodrigo3nz@gmail.com>
2022-03-27 21:53:15 -0300

    Navbar 2

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
     return(
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand fs-1 fst-italic text-secondary" href="#">W</a>
+    <a class="navbar-brand fs-1 fst-italic text-secondary" href="#">Watches</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
